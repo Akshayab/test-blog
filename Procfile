b28d35013b1a49859b1072efbef091ef1b6f8870
Akshay Budhkar <akshaybudhkar@gmail.com>
2014-08-09 14:27:56 -0400

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/test-blog-standalone.jar clojure.main -m test-blog.web
---
+++ test/test_blog/web_test.clj
@@ -0,0 +1,6 @@
+(ns test-blog.web-test
+  (:require [clojure.test :refer :all]
+            [test-blog.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
